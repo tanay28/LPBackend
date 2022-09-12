00b662640a4def7a51a4b4c6ae4c04969c34ea97
Tanay <tmtanay56@gmail.com>
2022-09-12 22:12:14 +0530

    added token validation user and activation deactivation

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const userController = require('../controllers/UserController');
 const { validateOpenRequest } = require('../auth/request-validation');
+const { checkToken } = require('../auth/Token_validation');
 
 router.post('/users', validateOpenRequest, userController.registerUser);
 router.get('/users', validateOpenRequest, userController.getAllUser);
-router.put('/users/activate', validateOpenRequest, userController.activateUser);
-router.put('/users/deactivate', validateOpenRequest, userController.deactivateUser);
 router.post('/users/forgotpassword', validateOpenRequest, userController.forgotPassword);
 router.post('/users/verifyuserotp', validateOpenRequest, userController.verifyUserOtp);
 router.put('/users/createnewpassword', validateOpenRequest, userController.createNewPassword);
+
+router.put('/users/activate', validateOpenRequest, checkToken, userController.activateUser);
+router.put('/users/deactivate', validateOpenRequest, checkToken, userController.deactivateUser);
 
 
 module.exports = router;
