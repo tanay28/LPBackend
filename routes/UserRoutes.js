ad5a29b6fc1b8a545cdf49ab1a35ec3869a0b5a7
Tanay <tmtanay56@gmail.com>
2022-10-02 09:18:10 +0530

    add token auth for user retrieve call

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const userController = require('../controllers/UserController');
 const { validateOpenRequest } = require('../auth/request-validation');
-const { authoriseAdminRoutes } = require('../auth/authorise_admin_routes');  
+const { authoriseAdminRoutes } = require('../auth/authorise_admin_routes');
+const { checkToken } = require('../auth/Token_validation');
 
 router.post('/users', validateOpenRequest, userController.registerUser);
-router.get('/users', validateOpenRequest, userController.getAllUser);
+router.get('/users', validateOpenRequest, checkToken, userController.getAllUser);
 router.post('/users/forgotpassword', validateOpenRequest, userController.forgotPassword);
 router.post('/users/verifyuserotp', validateOpenRequest, userController.verifyUserOtp);
 router.put('/users/createnewpassword', validateOpenRequest, userController.createNewPassword);
